413d0adcd15edfbf991287ecb29bc60f82611ccd
TrungNguyen0101 <tronzinmj213@gmail.com>
2024-05-26 11:34:35 +0700

    11111111

--- pages/404.tsx
+++ pages/404.tsx
@@ -50,9 +50,7 @@ const ButtonSearch = styled(Button)(
 function Status404() {
   return (
     <>
-      <Head>
-        <title>Status - 404</title>
-      </Head>
+      <title>Status - 404</title>
       <MainContent>
         <TopWrapper>
           <Container maxWidth="md">
